0298e1a5bfb9658dc0f1bf722d1c7fc0d2e49650
Muhammad Saad <github.5s32y@slmail.me>
2024-01-20 18:12:10 +0000

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Marriage Resume",
+  description: "Create a beautiful, personalized marriage resume to send to potential spouses. Powered by AI that helps you stand out. Get started now for free!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
