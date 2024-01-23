8b5850e78c05caf7ff3f1c7fe5a811bc659fcd77
Muhammad Saad <github.5s32y@slmail.me>
2024-01-23 04:56:41 +0100

    Improve header home button

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -2,15 +2,22 @@ import Link from "next/link"
 import { ModeToggle } from "../ui/mode-toggle"
 import { Button } from "../ui/button"
 import { SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import { HeartFilledIcon } from "@radix-ui/react-icons"
 
 const Header = () => {
   return (
     <header className="flex items-center justify-between gap-2 p-4">
-      <Link href="/">Logo Here</Link>
+      <Link href="/">
+        <Button variant="default" className="flex gap-2 pl-3">
+          <HeartFilledIcon />marriage resume
+        </Button>
+      </Link>
       <div className="flex items-center justify-center gap-2">
         <ModeToggle />
         <SignedIn>
-          <UserButton afterSignOutUrl="/"/>
+          <Button variant="outline" size="icon">
+            <UserButton afterSignOutUrl="/"/>
+          </Button>
         </SignedIn>
         <SignedOut>
           <SignUpButton>
