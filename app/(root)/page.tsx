879045bd9392467ac213253c611bd54a2f742a47
Muhammad Saad <github.5s32y@slmail.me>
2024-01-24 22:38:27 +0100

    Add mockup + fix responsiveness

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,26 +6,37 @@ import { SignUpButton } from "@clerk/nextjs"
 
 export default function Component() {
   return (
-      <section className="min-h-screen w-screen p-10 md:px-24 flex flex-col items-center justify-center bg-gradient-to-br from-transparent via-primary from-75% via-85% to-95% to-transparent">
-          <div className="max-w-7xl">
-            <Badge variant="secondary">NEW</Badge>
-            <span className="ml-2 text-sm">AI-powered profile suggestions 🎉</span>
-            <h3 className="max-w-md md:max-w-3xl mt-4 text-xl md:text-3xl lg:text-4xl tracking-tight leading-tight text-muted-foreground">
-              Say <i>Salam</i> to the modern way for Muslims to share their profiles with marriage aunties.
-            </h3>
-            <h1 className="mt-4 text-4xl md:text-6xl lg:text-7xl font-bold tracking-tight leading-none">
-              Create your beautiful, personalized profile to <span className="text-primary">stand out</span> now!
-            </h1>
-            <div className="mt-6 md:mt-10 flex items-center duration-300">
-              <p className="pr-1 pl-2 h-9 font-mono text-lg flex items-center rounded-l-full bg-primary font-medium text-background dark:text-foreground">marriageresu.me/</p>
-              <div className="flex items-center space-x-2 text-center">
-                <Input className="bg-background text-lg h-9 border-l-0 rounded-l-none p-2 w-32 md:w-56" placeholder="Saad"/>
-                <SignUpButton>
-                  <Button className="flex gap-2 items-center">Claim <ArrowTopRightIcon /></Button>
-                </SignUpButton>
-              </div>
+      <section className="min-h-screen w-screen pb-12 px-10 md:px-24 flex flex-col lg:flex-row gap-12 items-center justify-center bg-gradient-to-br from-transparent via-primary from-75% via-85% to-95% to-transparent">
+        <div className="mt-32 lg:mt-0 max-w-full md:max-w-2xl lg:max-w-3xl">
+          <Badge variant="secondary">NEW</Badge>
+          <span className="ml-2 text-sm">AI-powered profile suggestions 🎉</span>
+          <h3 className="mt-4 text-lg sm:text-xl md:text-2xl xl:text-4xl tracking-tight leading-tight text-muted-foreground">
+            Say <i>Salam</i> to the modern way for Muslims to share their profiles with marriage aunties.
+          </h3>
+          <h1 className="mt-4 text-3xl sm:text-4xl  md:text-5xl xl:text-7xl font-bold tracking-tight leading-none">
+            Create your beautiful, personalized profile to <span className="text-primary">stand out</span> now!
+          </h1>
+          <div className="mt-6 md:mt-10 flex flex-col md:flex-row items-center gap-2">
+            <div className="flex items-center">
+              <p className="pr-1 pl-2 h-9 font-mono text-base sm:text-lg flex items-center bg-secondary rounded-l-md font-medium text-foreground">marriageresu.me/</p>
+              <Input className="bg-background text-base sm:text-lg h-9 border-l-0 rounded-l-none w-36 sm:w-48 md:w-56" placeholder="Saad"/>
             </div>
+            <SignUpButton>
+                <Button className="flex w-[19.5rem] sm:w-[23.5rem] md:w-36 lg:w-auto gap-2 items-center">Claim <ArrowTopRightIcon /></Button>
+            </SignUpButton>
           </div>
+        </div>
+
+        <div className="relative mx-auto border-black bg-black border-[14px] rounded-[2.5rem] h-[600px] w-[300px] shadow-md shadow-muted">
+          <div className="w-[148px] h-[18px] bg-black top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 absolute"></div>
+          <div className="h-[46px] w-[3px] bg-black absolute -start-[17px] top-[124px] rounded-s-lg"></div>
+          <div className="h-[46px] w-[3px] bg-black absolute -start-[17px] top-[178px] rounded-s-lg"></div>
+          <div className="h-[64px] w-[3px] bg-black absolute -end-[17px] top-[142px] rounded-e-lg"></div>
+          <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px bg-black">
+            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mockup-2-light.png" className="dark:hidden w-[272px] h-[572px]" alt="Light theme phone mockup" />
+            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mockup-2-dark.png" className="hidden dark:block w-[272px] h-[572px]" alt="Dark theme phone mockup" />
+          </div>
+        </div>
       </section>
   )
 }
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -6,12 +6,12 @@ import Image from "next/image"
 
 const Header = () => {
   return (
-    <header className="fixed bg-background/80 z-10 self-center w-full rounded-lg border-b-2 flex items-center justify-between gap-2 p-4">
-      <Link href="/" className="flex gap-2 items-center justify-center">
+    <header className="fixed bg-background/95 z-10 self-center w-screen rounded-lg border-b-2 flex items-center justify-between gap-2 py-4">
+      <Link href="/" className="flex gap-2 items-center justify-center pl-4">
           <Image src="/favicon.png" alt="Marriage Resume" width={24} height={24}/>
           <p className="font-bold tracking-tight">MarriageResume</p>
       </Link>
-      <div className="flex items-center justify-center gap-2">
+      <div className="flex items-center justify-center gap-2 pr-4">
         <ModeToggle />
         <SignedIn>
           <Button variant="ghost" size="icon">
