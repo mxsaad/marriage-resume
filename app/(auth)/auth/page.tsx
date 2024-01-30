e4d9fdb39ada362ea1d6063685a4ca3c5f4d541b
Muhammad Saad <github.5s32y@slmail.me>
2024-01-30 20:13:55 +0100

    Finish auth UI

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -2,16 +2,19 @@
 
 import Image from "next/image"
 import SignUpForm from "@/components/shared/sign-up-form"
+import SignInForm from "@/components/shared/sign-in-form"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
+import SocialLoginButtons from "@/components/shared/social-login-buttons"
 
 export default function AuthenticationPage() {
-  let [isLoggingIn, setIsLoggingIn] = useState<boolean>(false)
+  let [isSigningUp, setIsSigningUp] = useState(true)
 
   return (
     <main className="w-screen min-h-screen">
       <div className="flex">
+        { /* Hero */}
         <section className="bg-gray-800 w-1/2 h-screen hidden lg:block relative border-r-2">
           <Link href="/" className="absolute top-6 left-6 flex items-center gap-2">
             <Image
@@ -30,20 +33,36 @@ export default function AuthenticationPage() {
             <footer className="text-sm mt-2">Abu Rijaal</footer>
           </blockquote>
         </section>
+
+        { /* Auth */}
         <section className="bg-background w-full lg:w-1/2 min-h-screen flex items-center justify-center">
           <div className="mx-auto flex w-2/3 flex-col justify-center gap-2 my-12 max-w-[350px]">
             <div className="flex flex-col text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                {isLoggingIn ? "Welcome Back!" : "Join MarriageResume"}
+                {isSigningUp ? "Join MarriageResume" : "Welcome Back"}
               </h1>
               <p className="text-center text-sm text-muted-foreground">
-                {isLoggingIn ? "Don't have an account? " : "Already have an account? "}
-                <Button variant="link" className="px-0" onClick={() => setIsLoggingIn(!isLoggingIn)}>
-                  {isLoggingIn ? "Sign up" : "Sign in"}
-                </Button>.
+                {isSigningUp ? "Already have an account? " : "Don't have an account? "}
+                <Button variant="link" className="px-0" onClick={() => setIsSigningUp(!isSigningUp)}>
+                  {isSigningUp ? "Sign in" : "Sign up"}
+                </Button>
               </p>
             </div>
-            <SignUpForm />
+            {/* Form */}
+            {isSigningUp ? <SignUpForm /> : <SignInForm />}
+            {/* Divider */}
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                  OR
+                </span>
+              </div>
+            </div>
+            {/* Social Login Buttons */}
+            <SocialLoginButtons />
           </div>
         </section>
       </div>
--- components/shared/sign-in-form.tsx
+++ components/shared/sign-in-form.tsx
@@ -16,18 +16,8 @@ import {
 
 // Form Schema
 const formSchema = z.object({
-  username: z.string()
-    .regex(/^[a-z,A-Z,0-9,_]+$/, { message: "Username can only contain letters, numbers, and underscores" })
-    .min(4, { message: "Username must be at least 4 characters long" }),
-  email: z.string().email({ message: "Please enter a valid email" }),
+  email: z.string(),
   password: z.string()
-    .regex(/^[a-z,A-Z,0-9,!@#$%^&*]+$/, { message: "Password can only contain letters, numbers, and !@#$%^&*" })
-    .min(8, { message: "Password must be at least 8 characters long" })
-    .max(32, { message: "Password must be at most 32 characters long" }),
-  confirmPassword: z.string()
-}).refine(data => data.password === data.confirmPassword, {
-  message: "Passwords do not match",
-  path: ["confirmPassword"],
 })
 
 export default function SignUpForm() {
@@ -35,10 +25,8 @@ export default function SignUpForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
       email: "",
       password: "",
-      confirmPassword: "",
     },
   })
   // Form Submission Handler
@@ -49,19 +37,6 @@ export default function SignUpForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="saad" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
         <FormField
           control={form.control}
           name="email"
@@ -81,19 +56,6 @@ export default function SignUpForm() {
           render={({ field }) => (
             <FormItem>
               <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="********" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="confirmPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirm Password</FormLabel>
               <FormControl>
                 <Input type="password" placeholder="********" {...field} />
               </FormControl>
