5a7c13c5deaf20028256ce8f69d8cd37bbabd6c3
Muhammad Saad <github.5s32y@slmail.me>
2024-02-01 19:52:57 +0100

    Update claim username flow

--- components/shared/claim-username.tsx
+++ components/shared/claim-username.tsx
@@ -1,7 +1,7 @@
 import { ArrowTopRightIcon } from "@radix-ui/react-icons"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { SignUpButton } from "@clerk/nextjs"
+import Link from "next/link"
 
 export default function ClaimUsername() {
   return (
@@ -10,9 +10,9 @@ export default function ClaimUsername() {
         <p className="px-2 h-9 font-mono text-base sm:text-lg flex items-center bg-secondary rounded-l-md font-medium text-foreground">marriageresu.me/@</p>
         <Input className="bg-background font-mono text-base sm:text-lg h-9 border-l-0 rounded-l-none w-full md:w-56" placeholder="saad" />
       </div>
-      <SignUpButton>
-        <Button className="flex w-full md:w-auto gap-2 items-center">Claim <ArrowTopRightIcon /></Button>
-      </SignUpButton>
+      <Link href="/profile" className="w-full md:w-auto">
+        <Button className="flex w-full gap-2 items-center">Claim <ArrowTopRightIcon /></Button>
+      </Link>
     </div>
   )
 }
